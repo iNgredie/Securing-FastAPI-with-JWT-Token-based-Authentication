158c5a6070299a21eb8d5c5a263eeb7d8b030010
Aleksandr Durnev <sandslash@bk.ru>
2021-02-22 20:55:13 +0400

    added securing routes

--- app/api.py
+++ app/api.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Depends
 
+from app.auth.auht_bearer import JWTBearer
 from app.auth.auht_handler import sign_jwt
 from app.model import PostSchema, UserSchema, UserLoginSchema
 
@@ -40,18 +41,9 @@ async def get_single_post(id: int) -> dict:
             }
 
 
-@app.post('/posts', tags=['posts'])
-async def add_post(post: PostSchema) -> dict:
-    post.id = len(posts) + 1
-    posts.append((post.dict()))
-    return {
-        'data': 'post added'
-    }
-
-
 @app.post('/user/signup', tags=['user'])
 async def create_user(user: UserSchema = Body(...)):
-    users.append(user) # replace with db call, making sure to hash the password first
+    users.append(user)  # replace with db call, making sure to hash the password first
     return sign_jwt(user.email)
 
 
@@ -63,9 +55,18 @@ def check_user(data: UserLoginSchema):
 
 
 @app.post('/user/login', tags=['user'])
-async def user_login(user:UserLoginSchema = Body(...)):
+async def user_login(user: UserLoginSchema = Body(...)):
     if check_user(user):
         return sign_jwt(user.email)
     return {
         'error': 'Wrong login details!'
+    }
+
+
+@app.post('/posts', dependencies=[Depends(JWTBearer())], tags=['post'])
+async def add_post(post: PostSchema) -> dict:
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {
+        'data': 'post added.'
     }
